f0e4d5ca7920eb0ba7d7474ffaca4685f3036fb5
Naftali Kizner <76939353+naftali10@users.noreply.github.com>
2023-09-01 20:03:39 +0300

    Added repop feature. Improved testing.

--- tests/test_Main.py
+++ tests/test_Main.py
@@ -7,9 +7,16 @@ class TestMain(unittest.TestCase):
     def setUp(self) -> None:
         self.under_test = Main(debug=True)
 
-    def test_run_keybind(self):
-        print("Please play with the GUI and alt+f11 keybind")
-        self.under_test.run()
+    def test_run(self):
+        print("Please play with the GUI and alt+f11 keybind\n"
+              "Times to expect:\n"
+              "\tMajor wait = 5 seconds\n"
+              "\tMinor wait = 2 seconds\n"
+              "\tBeeps to popup interval = 2 seconds\n"
+              "\tRepop interval = 10 seconds\n"
+              "Click Kill when you're done")
+        with self.assertRaises(RuntimeError):
+            self.under_test.run()
 
 
 if __name__ == '__main__':
